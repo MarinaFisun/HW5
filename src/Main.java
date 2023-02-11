aab098be488106d3d001b2f7560a449f1578ad78
Marina <m-fisun23@yandex.ru>
2023-02-12 00:41:27 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
     public static void task1() {
         System.out.println("Task 1");
@@ -10,6 +11,16 @@ public class Main {
         } else
         {
             System.out.println("Если возраст человека равен " + age + " , то он не достиг совершеннолетия, нужно немного подождать" );
+        }
+    }
+    public static void task2() {
+        System.out.println("Task 2");
+        int airTemperature = 5;
+        if (airTemperature < 5) {
+            System.out.println("На улице " + airTemperature+ " градусов, нужно надеть шапку");
+        }
+        if (airTemperature >= 5) {
+            System.out.println("На улице " + airTemperature + " градусов, можно идти без шапки");
         }
     }
 }
