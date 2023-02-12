d20ccbfc98abbc9adec167e3eef7b44d63419686
Marina <m-fisun23@yandex.ru>
2023-02-12 22:37:55 +0300

    task 4

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         task1();
         task2();
         task3();
+        task4();
     }
     public static void task1() {
         System.out.println("Task 1");
@@ -33,4 +36,20 @@ public class Main {
             System.out.println("Если скорость " + speed + " , то придется заплатить штраф");
         }
         }
+    public static void task4() {
+        System.out.println("Task 4");
+        int age = 19;
+        if (age >= 2 && age <= 6) {
+            System.out.println("Если возраст человека равен " + age + " , то ему нужно ходить в детский сад");
+        }
+        if (age > 6 && age <= 18) {
+            System.out.println("Если возраст человека равен " + age + " , то ему нужно ходить в школу");
+        }
+        if (age > 18 && age <= 24) {
+            System.out.println("Если возраст человека равен " + age + " , то ему нужно ходить в университет");
+        }
+        if (age > 24) {
+            System.out.println("Если возраст человека равен " + age + " , то ему нужно ходить на работу");
+        }
+    }
     }
